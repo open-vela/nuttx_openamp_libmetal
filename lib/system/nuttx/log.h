1bd0c8d29818452e819ebf92eeabf3d856a8b9c0
Bowen Wang <wangbowen6@xiaomi.com>
2025-04-22 10:54:42 +0800

    lib/system/nuttx: add a wrapper function to log handler
    Because syslog in nuttx may be macro, so add a wrapper function
    to cover all cases
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- lib/system/nuttx/log.h
+++ lib/system/nuttx/log.h
@@ -16,11 +16,22 @@
 #ifndef __METAL_NUTTX_LOG__H__
 #define __METAL_NUTTX_LOG__H__
 
+#include <stdarg.h>
 #include <syslog.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+static inline void metal_nuttx_log_handler(enum metal_log_level level,
+					   const char *format, ...)
+{
+	va_list ap;
+
+	va_start(ap, format);
+	vsyslog(level, format, ap);
+	va_end(ap);
+}
 
 #ifdef __cplusplus
 }
