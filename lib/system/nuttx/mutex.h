f211dd5ccadb595a2b0dfaafcd3609622c494100
Bowen Wang <wangbowen6@xiaomi.com>
2025-08-20 16:29:23 +0800

    lib/system/nuttx/mutex.h: support both the rspinlock and rmutex
    Because some modules need use this lock as the mutex and rpmsg virtio
    will use this as the spinlock, so support the metal_mutex_t as both
    the nuttx rspinlock or rmutex.
    
    Only Rpmsg VirtIO will use the metal_mutex_t as rspinlock, so
    Rpmsg VirtIO will set the is_spinlock to true after the rpmsg_init_vdev
    called.
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- lib/system/nuttx/mutex.h
+++ lib/system/nuttx/mutex.h
@@ -16,7 +16,10 @@
 #ifndef __METAL_NUTTX_MUTEX__H__
 #define __METAL_NUTTX_MUTEX__H__
 
+#include <stdbool.h>
+
 #include <metal/utilities.h>
+#include <nuttx/mutex.h>
 #include <nuttx/spinlock.h>
 
 #ifdef __cplusplus
@@ -24,15 +27,18 @@ extern "C" {
 #endif
 
 typedef struct metal_mutex {
+	bool is_spinlock;
 	rspinlock_t lock;
 	irqstate_t flags;
+	rmutex_t mutex;
 } metal_mutex_t;
 
 /*
  * METAL_MUTEX_INIT - used for initializing an mutex element in a static struct
  * or global
  */
-#define METAL_MUTEX_INIT(m) {.lock.val = 0, .flags = 0}
+#define METAL_MUTEX_INIT(m) \
+	{.is_spinlock = false, .lock.val = 0, .flags = 0, .mutex = NXRMUTEX_INITIALIZER}
 /*
  * METAL_MUTEX_DEFINE - used for defining and initializing a global or
  * static singleton mutex
@@ -42,6 +48,7 @@ typedef struct metal_mutex {
 static inline void __metal_mutex_init(metal_mutex_t *mutex)
 {
 	rspin_lock_init(&mutex->lock);
+	nxrmutex_init(&mutex->mutex);
 }
 
 static inline void __metal_mutex_deinit(metal_mutex_t *mutex)
@@ -54,9 +61,13 @@ static inline int __metal_mutex_try_acquire(metal_mutex_t *mutex)
 	irqstate_t flags;
 	int ret;
 
-	ret= rspin_trylock_irqsave_nopreempt(&mutex->lock, flags);
-	if (ret && !rspin_lock_is_recursive(&mutex->lock))
-		mutex->flags = flags;
+	if (mutex->is_spinlock) {
+		ret = rspin_trylock_irqsave_nopreempt(&mutex->lock, flags);
+		if (ret && !rspin_lock_is_recursive(&mutex->lock))
+			mutex->flags = flags;
+	} else {
+		ret = nxrmutex_trylock(&mutex->mutex);
+	}
 
 	return ret;
 }
@@ -65,19 +76,29 @@ static inline void __metal_mutex_acquire(metal_mutex_t *mutex)
 {
 	irqstate_t flags;
 
-	flags = rspin_lock_irqsave_nopreempt(&mutex->lock);
-	if (!rspin_lock_is_recursive(&mutex->lock))
-		mutex->flags = flags;
+	if (mutex->is_spinlock) {
+		flags = rspin_lock_irqsave_nopreempt(&mutex->lock);
+		if (!rspin_lock_is_recursive(&mutex->lock))
+			mutex->flags = flags;
+	} else {
+		nxrmutex_lock(&mutex->mutex);
+	}
 }
 
 static inline void __metal_mutex_release(metal_mutex_t *mutex)
 {
-	rspin_unlock_irqrestore_nopreempt(&mutex->lock, mutex->flags);
+	if (mutex->is_spinlock)
+		rspin_unlock_irqrestore_nopreempt(&mutex->lock, mutex->flags);
+	else
+		nxrmutex_unlock(&mutex->mutex);
 }
 
 static inline int __metal_mutex_is_acquired(metal_mutex_t *mutex)
 {
-	return rspin_lock_is_locked(&mutex->lock);
+	if (mutex->is_spinlock)
+		return rspin_lock_is_locked(&mutex->lock);
+	else
+		return nxrmutex_is_locked(&mutex->mutex);
 }
 
 #ifdef __cplusplus
